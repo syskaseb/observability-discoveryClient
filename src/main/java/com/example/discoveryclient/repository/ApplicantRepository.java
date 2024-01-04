86ab7107578baa4637b5e426297b9a630fdfd171
syskas01 <syskas01@stepstone.com>
2024-01-04 21:11:39 +0100

    fix hibernate-tools

--- src/main/java/com/example/discoveryclient/repository/ApplicantRepository.java
+++ src/main/java/com/example/discoveryclient/repository/ApplicantRepository.java
@@ -1,5 +1,6 @@
-package com.example.discoveryclient;
+package com.example.discoveryclient.repository;
 
+import com.example.discoveryclient.model.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
