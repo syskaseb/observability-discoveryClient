80f1ed88b47900e5cb18a3ecaad13f4139b8c56b
syskas01 <syskas01@stepstone.com>
2024-01-23 14:07:25 +0100

    change changesets format from sql to xml

--- src/main/resources/db/changelog/changes/002_change_dates_to_timestamp.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset syskas01:002_change_dates_to_timestamp
-ALTER TABLE application
-ALTER COLUMN application_date TYPE TIMESTAMP(4);
-
-ALTER TABLE job_offer
-ALTER COLUMN posted_date TYPE TIMESTAMP(4);
