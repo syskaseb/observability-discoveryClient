5e28c2498eefbb86452a67b785dbb413c0506cb2
syskaseb <syska.seb@gmail.com>
2025-01-05 03:56:29 +0100

    change temporarily swagger server url to api gateway
    Signed-off-by: syskaseb <syska.seb@gmail.com>

--- blocking-api/src/main/java/com/example/blockingapi/config/auth/OpenAPIConfiguration.java
+++ blocking-api/src/main/java/com/example/blockingapi/config/auth/OpenAPIConfiguration.java
@@ -5,18 +5,25 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
-@OpenAPIDefinition(security = { @SecurityRequirement(name = "bearer-key") })
+@OpenAPIDefinition(security = {@SecurityRequirement(name = "bearer-key")})
 public class OpenAPIConfiguration {
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("http://localhost:8077")))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
