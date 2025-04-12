c802400c850686d343730fb23abbb4d302bd4155
syskaseb <syska.seb@gmail.com>
2025-04-13 00:55:57 +0200

    chore: add app info to the openapi spec config

--- blocking-api/src/main/java/com/example/blockingapi/config/auth/OpenAPIConfiguration.java
+++ blocking-api/src/main/java/com/example/blockingapi/config/auth/OpenAPIConfiguration.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +17,16 @@ import java.util.List;
 @OpenAPIDefinition(security = {@SecurityRequirement(name = "bearer-key")})
 public class OpenAPIConfiguration {
 
+    @Value("${app.version}")
+    private String appVersion;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("Blocking API")
+                        .version(appVersion)
+                        .description("The Blocking API documentation"))
                 .servers(List.of(new Server().url("http://localhost:8077")))
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
